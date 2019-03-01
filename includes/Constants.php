ec6660f7eeb5af2447308d5cc7aa3c04370d73e0
Lachy Schuamcher <32254417+tydewest@users.noreply.github.com>
2019-03-01 19:43:10 +1100

    Add files via upload

---
+++ includes/Constants.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lachy
+ * Date: 3/7/18
+ * Time: 1:03 AM
+ */
+
+
+define('DB_USERNAME', 'user');
+define('DB_PASSWORD', 'Password1');
+define('DB_HOST', 'server121.web-hosting.com');
+define('DB_NAME', 'servhkuf_users');
+
+define('USER_CREATED', 0);
+define('USER_ALREADY_EXIST', 1);
+define('USER_NOT_CREATED', 2);
